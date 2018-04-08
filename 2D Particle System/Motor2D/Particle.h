8a6838b9d7ce9116b296cff75c5ed473007c4f9e
nintervik <vmasogarcia@gmail.com>
2018-04-08 14:28:39 +0200

    cleaned particle header

--- 2D Particle System/Motor2D/Particle.h
+++ 2D Particle System/Motor2D/Particle.h
@@ -17,14 +17,12 @@ class Particle
 		fPoint pos = { 250.0f, 200.0f };
 		float speed = 25.0f;
 		float scale = 30.0f;
-	};
-
-	Vortex vortex;
+	} vortex;
 
 	union ParticleInfo
 	{
-		// This struct holds the state of the particle when 
-		// it's being update (it's still alive).
+		/* This struct holds the state of the particle when 
+		   it's being update (it's still alive).*/
 		struct ParticleState
 		{
 			fPoint pos;
@@ -45,10 +43,10 @@ class Particle
 
 		} pLive;
 
-		// If the particle is dead, then the 'next' member comes 
-		// into play and the struct it's not used. This pointer
-		// called 'next' holds a pointer to the next available 
-		// particle after this one.
+		/* If the particle is dead, then the 'next' member comes 
+		   into play and the struct it's not used. This pointer
+		   called 'next' holds a pointer to the next available 
+		   particle after this one. */
 		Particle* next;
 
 		ParticleInfo() {}
@@ -57,17 +55,23 @@ class Particle
 	public:
 
 	Particle();
+
+	// Initializes new generated particle
 	void Init(fPoint pos, float speed, float angle, double rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode);
 
+	// Generic methods
 	void Update(float dt);
 	void Draw();
-
 	bool IsAlive();
 
+	// Particle pointer methods
 	Particle* GetNext();
 	void SetNext(Particle* next);
 
+	// Given two colors interpolates linearly over time and returns the resulting color
 	SDL_Color RgbInterpolation(SDL_Color startColor, SDL_Color endColor, float timeStep);
+
+	// Vortex methods
 	void AddVortex(fPoint pos, float speed, float scale);
 	void CalculatePosFromVortex(float dt);
 };
