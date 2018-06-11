0a61de73755b0f13d82c8c7ee7e6df3749b3cd03
nintervik <vmasogarcia@gmail.com>
2018-06-11 21:28:02 +0200

    Solved memory leaks in texture module

--- full_code/2D Particle System/Motor2D/j1Textures.cpp
+++ full_code/2D Particle System/Motor2D/j1Textures.cpp
@@ -17,7 +17,7 @@ j1Textures::~j1Textures()
 {}
 
 // Called before render is available
-bool j1Textures::Awake(pugi::xml_node& config)
+bool j1Textures::Awake(pugi::xml_node& texturesNode)
 {
 	LOG("Init Image library");
 	bool ret = true;
@@ -25,7 +25,7 @@ bool j1Textures::Awake(pugi::xml_node& config)
 	int flags = IMG_INIT_PNG;
 	int init = IMG_Init(flags);
 
-	if((init & flags) != flags)
+	if ((init & flags) != flags)
 	{
 		LOG("Could not initialize Image lib. IMG_Init: %s", IMG_GetError());
 		ret = false;
@@ -64,7 +64,7 @@ SDL_Texture* const j1Textures::Load(const char* path)
 	SDL_Texture* texture = NULL;
 	SDL_Surface* surface = IMG_Load(path);
 
-	if(surface == NULL)
+	if (surface == NULL)
 	{
 		LOG("Could not load surface with path: %s. IMG_Load: %s", path, IMG_GetError());
 	}
@@ -74,6 +74,11 @@ SDL_Texture* const j1Textures::Load(const char* path)
 		SDL_FreeSurface(surface);
 	}
 
+	if (texture != NULL)
+		textures.push_back(texture);
+	else
+		return NULL;
+
 	return texture;
 }
 
@@ -82,9 +87,9 @@ bool j1Textures::UnLoad(SDL_Texture* texture)
 {
 	std::list<SDL_Texture*>::const_iterator item;
 
-	for (item = textures.begin(); item != textures.end(); item = ++item)
+	for (item = textures.begin(); item != textures.end(); ++item)
 	{
-		if (texture == *item)
+		if (texture == (*item))
 		{
 			SDL_DestroyTexture(*item);
 			textures.remove(*item);
@@ -100,20 +105,18 @@ SDL_Texture* const j1Textures::LoadSurface(SDL_Surface* surface)
 {
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(App->render->renderer, surface);
 
-	if(texture == NULL)
+	if (texture == NULL)
 	{
 		LOG("Unable to create texture from surface! SDL Error: %s\n", SDL_GetError());
 	}
 	else
-	{
-		textures.remove(texture);
-	}
+		return texture;
 
-	return texture;
+	return NULL;
 }
 
 // Retrieve size of a texture
 void j1Textures::GetSize(const SDL_Texture* texture, uint& width, uint& height) const
 {
-	SDL_QueryTexture((SDL_Texture*)texture, NULL, NULL, (int*) &width, (int*) &height);
+	SDL_QueryTexture((SDL_Texture*)texture, NULL, NULL, (int*)&width, (int*)&height);
 }
--- full_code/2D Particle System/Motor2D/j1Textures.h
+++ full_code/2D Particle System/Motor2D/j1Textures.h
@@ -12,20 +12,12 @@ class j1Textures : public j1Module
 public:
 
 	j1Textures();
-
-	// Destructor
 	virtual ~j1Textures();
 
-	// Called before render is available
 	bool Awake(pugi::xml_node&);
-
-	// Called before the first frame
 	bool Start();
-
-	// Called before quitting
 	bool CleanUp();
 
-	// Load Texture
 	SDL_Texture* const	Load(const char* path);
 	bool				UnLoad(SDL_Texture* texture);
 	SDL_Texture* const	LoadSurface(SDL_Surface* surface);
@@ -33,8 +25,8 @@ public:
 
 public:
 
-	std::list<SDL_Texture*>	textures;
+	std::list<SDL_Texture*> textures;
 };
 
 
-#endif // __j1TEXTURES_H__
+#endif
